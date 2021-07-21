d8f524127eb32bb878d85e335d2b09b44b1e2034
Matheus Morais <gm80648@gmail.com>
2021-07-20 22:46:31 -0300

    QuestionElement refactoring

--- src/components/QuestionElement/index.tsx
+++ src/components/QuestionElement/index.tsx
@@ -4,12 +4,23 @@ import { Profile } from "../Profile";
 
 import "./styles.scss";
 
+interface IQuestion extends Question {
+  [key: string]: any;
+}
+
 export const QuestionElement: React.FC<{
-  question: Question;
+  question: IQuestion;
   actions: React.ReactNode;
 }> = ({ question, actions }) => {
+  let className = "question";
+  const variations = ["hidden", "highlighted"];
+
+  variations.forEach((variation) => {
+    if (question[variation]) className += ` ${variation}`;
+  });
+
   return (
-    <div className="question">
+    <div className={className}>
       <p>{question.content}</p>
       <div className="question-bottom">
         <Profile user={question.user} />
