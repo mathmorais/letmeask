e72a86d3e3c34d5ba8a44737ade346b6513c5b1c
Matheus Morais <gm80648@gmail.com>
2021-07-20 22:58:23 -0300

    Add Like method in question actions

--- src/hooks/useQuestionActions.ts
+++ src/hooks/useQuestionActions.ts
@@ -1,46 +1,59 @@
 import { useDatabase } from "../hooks/useDatabase";
 import firebase from "firebase/app";
 import { useGetRoomUid } from "./useGetRoomUid";
+import { Like } from "../entities/Like";
+import { useAuthContext } from "./useAuthContext";
 
 export const useQuestionActions = (questionUid: string) => {
   const database = useDatabase();
   const roomId = useGetRoomUid();
+  const { user } = useAuthContext();
 
   const handleGetQuestionRef = () => {
     return database.ref(`/rooms/${roomId}/questions`).child(questionUid);
   };
 
-  const handleHighlightQuestion = (
+  const handleHighlightQuestion = async (
     questionRef: firebase.database.Reference,
     prevValue?: boolean
-  ) => questionRef.child("isHighlighted").set(!prevValue);
+  ) => {
+    await questionRef.child("hidden").set(false);
+    await questionRef.child("highlighted").set(!prevValue);
+  };
 
-  const handleHideQuestion = (
+  const handleHideQuestion = async (
     questionRef: firebase.database.Reference,
     prevValue?: boolean
-  ) => questionRef.child("isHidden").set(!prevValue);
+  ) => {
+    await questionRef.child("highlighted").set(false);
+    questionRef.child("hidden").set(!prevValue);
+  };
 
-  const handleDeleteQuestion = async (
-    questionRef: firebase.database.Reference
-  ) => questionRef.remove();
+  const handleDeleteQuestion = (questionRef: firebase.database.Reference) =>
+    questionRef.remove();
+
+  const handleLikeQuestion = (questionRef: firebase.database.Reference) => {
+    if (user) {
+      questionRef.child(`likes/${user.uid}`).set(new Like());
+    } else {
+      alert("Usuário precisa estar logado");
+    }
+  };
 
   const actions = {
     highlight: handleHighlightQuestion,
     hide: handleHideQuestion,
     delete: handleDeleteQuestion,
+    like: handleLikeQuestion,
   } as { [actionName: string]: Function };
 
-  // const handleResetActions = async (
-  //   questionRef: firebase.database.Reference
-  // ) => {
-  //   await questionRef.child("isHighlighted").set(false);
-  //   await questionRef.child("isHidden").set(false);
-  // };
-
-  const handleQuestionAction = async (action: string, prevValue?: boolean) => {
+  const handleQuestionAction = async (
+    action: keyof typeof actions,
+    prevValue?: boolean
+  ) => {
     const ref = handleGetQuestionRef();
 
-    return actions[action](ref, prevValue);
+    await actions[action](ref, prevValue);
   };
 
   return { handleQuestionAction };
