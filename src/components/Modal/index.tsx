e29eaeab287cb99d92254c755ae7f2d70027aa61
Matheus Morais <gm80648@gmail.com>
2021-07-20 22:42:33 -0300

    Added modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,3 +1,37 @@
-export const Modal = () => {
-  return <div></div>;
+import { useState } from "react";
+import { Button } from "../Button";
+
+import "./styles.scss";
+
+interface IModalProps {
+  title: string;
+  description: string;
+  icon?: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  onAccept: () => void;
+}
+
+export const Modal: React.FC<IModalProps> = (props) => {
+  const [accepted, setAccepted] = useState(false);
+
+  if (!accepted) {
+    return (
+      <section id="modal-container">
+        <div>
+          {props.icon && <props.icon />}
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+          <div className="modal-actions">
+            <Button onClick={() => setAccepted(false)} variation={"cancel"}>
+              Cancel
+            </Button>
+            <Button onClick={props.onAccept} variation={"danger"}>
+              Sim, {props.title.split(" ")[0].toLowerCase()}
+            </Button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  return null;
 };
