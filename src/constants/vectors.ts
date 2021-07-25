ecc1a85642d31a7ce65c6edfd72289733709a4dd
Matheus Morais <gm80648@gmail.com>
2021-07-25 18:38:59 -0300

    Add vectors constants

---
+++ src/constants/vectors.ts
@@ -0,0 +1,21 @@
+import { ReactComponent as AnswerSvg } from "../assets/vectors/answer.svg";
+import { ReactComponent as CheckSvg } from "../assets/vectors/check.svg";
+import { ReactComponent as CopySvg } from "../assets/vectors/copy.svg";
+import { ReactComponent as DeleteSvg } from "../assets/vectors/delete.svg";
+import { ReactComponent as EmptyQuestionSvg } from "../assets/vectors/empty-question.svg";
+import { ReactComponent as GoogleIconSvg } from "../assets/vectors/google-icon.svg";
+import { ReactComponent as IllustrationSvg } from "../assets/vectors/illustration.svg";
+import { ReactComponent as LikeSvg } from "../assets/vectors/like.svg";
+import { ReactComponent as LogoSvg } from "../assets/vectors/logo.svg";
+
+export {
+  AnswerSvg,
+  CheckSvg,
+  CopySvg,
+  DeleteSvg,
+  EmptyQuestionSvg,
+  GoogleIconSvg,
+  IllustrationSvg,
+  LikeSvg,
+  LogoSvg,
+};
