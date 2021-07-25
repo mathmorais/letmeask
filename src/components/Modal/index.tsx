dd290595641b16c71b4eefada929759a046deae1
Matheus Morais <gm80648@gmail.com>
2021-07-25 18:43:28 -0300

    Add `Modal` animations

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,31 +1,38 @@
 import { useContext } from "react";
 import { ModalContext } from "../../contexts/ModalContext";
 import { Button } from "../Button";
+import { animated, config, useTransition } from "react-spring";
 
 import "./styles.scss";
 
 export const Modal = () => {
   const { modal, setModal } = useContext(ModalContext);
 
-  if (modal) {
-    return (
-      <section id="modal-container">
-        <div>
-          {modal.icon && <modal.icon />}
-          <h2>{modal.title}</h2>
-          <p>{modal.description}</p>
-          <div className="modal-actions">
-            <Button onClick={() => setModal(undefined)} variation={"cancel"}>
-              Cancel
-            </Button>
-            <Button onClick={modal.onAccept} variation={"danger"}>
-              Sim, {modal.title.split(" ")[0].toLowerCase()}
-            </Button>
-          </div>
-        </div>
-      </section>
-    );
-  }
+  const transition = useTransition(modal, {
+    from: { filter: "opacity(0)" },
+    enter: { filter: "opacity(1)" },
+    config: config.stiff,
+    expires: modal === undefined,
+  });
 
-  return null;
+  return transition(
+    (style, item) =>
+      item && (
+        <animated.section style={style} id="modal-container">
+          <div>
+            {item.icon && <item.icon />}
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+            <div className="modal-actions">
+              <Button onClick={() => setModal(undefined)} variation={"cancel"}>
+                Cancel
+              </Button>
+              <Button onClick={item.onAccept} variation={"danger"}>
+                Sim, {item.title.split(" ")[0].toLowerCase()}
+              </Button>
+            </div>
+          </div>
+        </animated.section>
+      )
+  );
 };
